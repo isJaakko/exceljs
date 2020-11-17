5fa6e25553559bd9c7e523a9bca355077750b953
yinjunjie <yinjunjie.yjj@bytedance.com>
2020-11-18 00:11:37 +0800

    feat: 高亮标记正确答案单元格

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const ExcelJS = require("exceljs");
+const path = require("path");
+const fs = require("fs");
+
+const filename = path.resolve(__dirname, "../data", "data.xlsx");
+const readFile = async () => {
+  const options = {
+    sharedStrings: "emit",
+    hyperlinks: "emit",
+    worksheets: "emit",
+  };
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile(filename);
+
+  // 访问工作表
+  workbook.eachSheet(function (worksheet, sheetId) {
+    worksheet.eachRow(function (row, rowNumber) {
+      const answers = row.getCell(8);
+      const pattern = /[a-dA-D]/g;
+      const answerMap = {
+        A: "D",
+        B: "E",
+        C: "F",
+        D: "G",
+        a: "D",
+        b: "E",
+        c: "F",
+        d: "G",
+      };
+
+      if (pattern.test(answers)) {
+        answers
+          .toString()
+          .split("")
+          .forEach((answer) => {
+            const Char = answerMap[answer];
+            const index = `${Char}${rowNumber}`;
+
+            if (!Char) {
+              return;
+            }
+
+            console.log(index);
+
+            worksheet.getCell(index).style = {
+              fill: {
+                type: "pattern",
+                pattern: "solid",
+                fgColor: { argb: "FFFFFF00" },
+                bgColor: { argb: "FF00FF00" },
+              },
+            };
+          });
+      }
+    });
+  });
+
+  await workbook.xlsx.writeFile(filename);
+};
+
+readFile();
